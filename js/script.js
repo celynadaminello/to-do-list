387380364b0b3655f79655314699c65771626363
CelynaR <celynar@craisasede.corp>
2022-01-03 15:59:44 -0300

    to-do-list

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
-function mozaovaimemamar(t) {
-    let valor = t.value;
-    console.log(valor);
-    valor.innerText = "";
+onload = function(){
+
+    let container = localStorage.getItem("container");
+    let content = document.getElementById("container");
+    content.innerHTML = container;
 }
+     
+    function txtList(t) {
+
+    let addTxt = t.value;
+    console.log(addTxt);
+    
+    let content = document.getElementById("container");
+    content.innerHTML = addTxt;
+
+    let taskList = document.createElement("li");
+    taskList.innerHTML = addTxt;
+
+    var texto = document.createTextNode(addTxt);
+
+    localStorage.setItem("container", addTxt);
+    
+    
+} 
+
+/*function clearButtom(e){
+
+    let erase = e.value;
+    let erase1 = document.getElementById("buttom");
+    localStorage.clear(this.getElementById("container"));
+} */
